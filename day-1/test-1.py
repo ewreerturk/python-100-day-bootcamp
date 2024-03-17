c1dc8f89544c990b45aa6cb805bb289e1841f1b1
Emre Erturk <emre757talha@hotmail.com>
2024-03-18 01:32:43 +0300

    Add day 1 files

---
+++ day-1/test-1.py
@@ -0,0 +1,16 @@
+print ("Merhaba Hepiniz hos geldiniz")
+print ("""####
+Buyudugunuz sehir nedir?
+####""")
+x = input()
+print ("""####
+Evcil hayvaninizin adi nedir?
+####""")
+y = input()
+print ("Sizin band name'iniz", x + " " + y )
+
+
+print ("Welcome to the band name generator.")
+city = input ("Which city did you grow up in?\n" )
+pet = input("What is the name of a pen?\nx" )
+print("Your band name could be ", city + " " + pet)
