79f2475c4d1ebf20a51766f23038d23b1853749b
Emre Erturk <emre757talha@hotmail.com>
2024-03-19 00:25:50 +0300

    day 4 added

--- day-2/test-1.py
+++ day-2/test-1.py
@@ -14,7 +14,7 @@ height = input()
 # 2nd input: enter weight in kilograms e.g: 72
 weight = input()
 # 🚨 Don't change the code above 👆
-
+👈
 # Write your code below this line 👇
 height =float(height)
 weight =float(weight)
