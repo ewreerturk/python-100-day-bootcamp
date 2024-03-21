d77635889ad0650e27b2bb9456f3473dc71c43c5
Emre Erturk <emre757talha@hotmail.com>
2024-03-22 01:49:40 +0300

    day-5, day-6 and some reeborg's world codes added

---
+++ day-6/test-1.py
@@ -0,0 +1,17 @@
+#reborg code
+'''
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def yukari():
+    turn_left()
+    
+def sagadon():
+    turn_left()
+    turn_left()
+    turn_left()
+'''
+
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
---
+++ day-6/test-2.py
@@ -0,0 +1,26 @@
+#reborg codev2
+''''
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def yukari():
+    turn_left()
+    
+def sagadon():
+    turn_left()
+    turn_left()
+    turn_left()
+for _ in range(6):
+    move()
+    turn_left()
+    move()
+    sagadon()
+    move()
+    sagadon()
+    move()
+    turn_left()
+
+
+'''
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
